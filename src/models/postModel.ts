c15303cdf1e5659bf6fcfc6a60115dcb5204c388
Alexey Muravyov <muravyov.alexey@gmail.com>
2025-07-16 14:32:43 +0000

    fix: correct logic in generatePostId and clean up createPost and deletePost functions

--- src/models/postModel.ts
+++ src/models/postModel.ts
@@ -30,18 +30,15 @@ function generatePostId(posts: Post[]): string {
     let postId: string
     do {
         postId = randomUUID()
-    } while (posts.findIndex(p => p.id === postId) === -1)
+    } while (posts.findIndex(p => p.id === postId) !== -1)
     return postId
-
 }
 export async function createPost(postDraft: Omit<Post, 'id'>): Promise<Post> {
     let _posts = await loadPosts()
-
     let post: Post = {
         ...postDraft,
         id: generatePostId(_posts)
     }
-
     // validate post that slug not conflict with another post
     if (post.slug) {
         const conflictPost = _posts.find(p => p.slug === post.slug);
@@ -51,6 +48,8 @@ export async function createPost(postDraft: Omit<Post, 'id'>): Promise<Post> {
         }
     }
 
+    _posts = [..._posts, post]
+    
     try {
         await writeFile(dataPath, JSON.stringify(_posts, null, 4))
         posts = _posts
@@ -101,10 +100,6 @@ export async function deletePost(postId: string): Promise<void> {
     try {
         await writeFile(dataPath, JSON.stringify(_posts, null, 4))
         posts = _posts
-        console.log('DELETE POST')
-        console.log('posts:', JSON.stringify(_posts, null, 2))
-        console.log('post:', JSON.stringify(postId, null, 2))
-
     } catch (err) {
         console.error(err)
         throw err
