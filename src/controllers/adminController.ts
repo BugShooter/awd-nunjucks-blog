88427cc0591bf3e8b52a83a456dce2493a9665c8
Alexey Muravyov <muravyov.alexey@gmail.com>
2025-07-16 11:33:54 +0000

    feat: implement CRUD operations for posts in admin panel

--- src/controllers/adminController.ts
+++ src/controllers/adminController.ts
@@ -1,6 +1,6 @@
 import express from "express"
 import type { Router, Request, Response, NextFunction } from "express"
-import { posts, updatePost } from "../models/postModel.js";
+import { createPost, deletePost, posts, updatePost } from "../models/postModel.js";
 
 export const adminRouter: Router = express.Router();
 
@@ -28,6 +28,30 @@ adminRouter.get('/', (req: Request, res: Response) => {
     })
 })
 
+adminRouter.get('/post/create', (req: Request, res: Response) => {
+    const post: Post = {
+        title: '',
+        content: '',
+        date: new Date().getTime().toString()
+    }
+    //TODO: create admin/postCreate.html file with create form
+    // with post on /post
+    res.render('admin/postEdit.html', {
+        post
+    })
+})
+adminRouter.post('/post', async (req: Request, res: Response) => {
+    // TODO: validate req.body
+    const postDraft = {
+        slug: req.body.slug,
+        title: req.body.title,
+        content: req.body.content,
+        date: req.body.date,
+    }
+    post = await createPost(postDraft)
+    res.redirect(`/admin/post/${post.slug ? post.slug : post.id}`)
+})
+
 adminRouter.get('/post/:postId', findPostByParamMiddleware, (req: Request, res: Response) => {
     res.render('admin/post.html', {
         post
@@ -41,10 +65,22 @@ adminRouter.get('/post/:postId/edit', findPostByParamMiddleware, (req: Request, 
 })
 adminRouter.put('/post/:postId', findPostByParamMiddleware, async (req: Request, res: Response) => {
     if (!post) throw new Error('Unpossible case')
+    // TODO: validate fields and transform if necessary
+    if ('slug' in req.body) post.slug = req.body.slug
+    if ('title' in req.body) post.title = req.body.title
+    if ('content' in req.body) post.content = req.body.content
+    if ('date' in req.body) post.date = req.body.date
+
     await updatePost(post)
-    res.render('admin/postEdit.html', {
-        post
-    })
+    console.log(`Redirect: ${post.slug ? post.slug : post.id}`)
+    res.redirect(`${post.slug ? post.slug : post.id}`)
+})
+
+adminRouter.delete('/post/:postId', findPostByParamMiddleware, async (req: Request, res: Response) => {
+    if (!post || !post.id) throw new Error('Unpossible case')
+
+    await deletePost(post.id)
+    res.redirect('/admin')
 })
 
 adminRouter.use((req: Request, res: Response) => {
--- src/models/postModel.ts
+++ src/models/postModel.ts
@@ -1,24 +1,112 @@
-import fs from 'fs'
-import path from 'path'
+import path from 'node:path'
+import { access, constants, readFile, unlink, writeFile } from 'node:fs/promises'
+import type { UUID } from 'node:crypto'
+import { randomUUID } from 'node:crypto'
 import slug from 'slug'
 
-const rawPosts: Post[] = JSON.parse(fs.readFileSync(path.join(import.meta.dirname, '../data/posts.json'), 'utf-8'))
-export const posts = rawPosts.map((p: Post, i: number) => {
-    return {
-        ...p,
-        slug: slug(p.title),
-        id: String(i + 1)
-    }
-})
+const dataPath = path.join(import.meta.dirname, '../data/posts.json')
+export let posts: Post[] = await loadPosts()
 
-export async function createPost(post: Omit<Post, 'id'>): Promise<Post> {
-    throw new Error('TODO: implement postModel.createPost')
+export async function refresh(): Promise<void> {
+    posts = await loadPosts()
+}
+
+function enrichPosts(rawPosts: Post[]): Post[] {
+    return rawPosts.map((p: Post, i: number) => {
+        return {
+            ...p,
+            slug: p.slug ?? slug(p.title),
+            id: p.id ?? String(i + 1)
+        }
+    })
+}
+
+async function loadPosts(): Promise<Post[]> {
+    const json = await readFile(dataPath, 'utf-8')
+    const rawPosts: Post[] = JSON.parse(json)
+    return enrichPosts(rawPosts);
+}
+function generatePostId(posts: Post[]): string {
+    let postId: string
+    do {
+        postId = randomUUID()
+    } while (posts.findIndex(p => p.id === postId) === -1)
+    return postId
+
+}
+export async function createPost(postDraft: Omit<Post, 'id'>): Promise<Post> {
+    let _posts = await loadPosts()
+
+    let post: Post = {
+        ...postDraft,
+        id: generatePostId(_posts)
+    }
+
+    // validate post that slug not conflict with another post
+    if (post.slug) {
+        const conflictPost = _posts.find(p => p.slug === post.slug);
+        if (conflictPost) {
+            // TODO: create ValidationError and catch it in Controller
+            throw new Error(`This slug is already used by another post.\nSlug:"${post.slug}"\n Current ID:${post.id}, Conflic ID:${conflictPost?.id}`,)
+        }
+    }
+
+    try {
+        await writeFile(dataPath, JSON.stringify(_posts, null, 4))
+        posts = _posts
+        return post
+    } catch (err) {
+        console.error(err)
+        throw err
+    }
 }
 
 export async function updatePost(post: Post): Promise<boolean> {
-    throw new Error('TODO: implement postModel.savePost')
+    if (!post.id) {
+        // TODO: create ValidationError and catch it in Controller
+        throw new Error('Cannot update post without ID')
+    }
+
+    let _posts = await loadPosts()
+
+    // validate post that slug not conflict with another post
+    if (post.slug) {
+        const conflictPost = _posts.find(p => p.slug === post.slug && p.id !== undefined && p.id !== post.id);
+        if (conflictPost) {
+            // TODO: create ValidationError and catch it in Controller
+            throw new Error(`This slug is already used by another post.\nSlug:"${post.slug}"\n Current ID:${post.id}, Conflic ID:${conflictPost?.id}`,)
+        }
+    }
+
+    _posts = _posts.filter(p => p.id !== post.id)
+    _posts = [..._posts, post]
+    try {
+        await writeFile(dataPath, JSON.stringify(_posts, null, 4))
+        posts = _posts
+        return true
+    } catch (err) {
+        console.error(err)
+    }
+    return false
 }
 
 export async function deletePost(postId: string): Promise<void> {
-    throw new Error('TODO: implement postModel.deletePost')
+    let _posts = await loadPosts()
+
+    if (_posts.findIndex(p => p.id === postId) === -1) {
+        // Post with postID is not exist
+        return
+    }
+    _posts = _posts.filter(p => p.id !== postId)
+    try {
+        await writeFile(dataPath, JSON.stringify(_posts, null, 4))
+        posts = _posts
+        console.log('DELETE POST')
+        console.log('posts:', JSON.stringify(_posts, null, 2))
+        console.log('post:', JSON.stringify(postId, null, 2))
+
+    } catch (err) {
+        console.error(err)
+        throw err
+    }
 }
